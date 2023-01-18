9ee51c970e8847a9ca828776bb5a0f6de34f3194
Jessica Whitney <jessica.whitney.22@ucl.ac.uk>
2023-01-18 15:45:57 +0000

    Updating the analysis tests

--- tests/test_reality.py
+++ tests/test_reality.py
@@ -7,7 +7,7 @@ N_to_test = [4, 6, 8]
 J_min_to_test = [0]
 lam_to_test = [2, 3]
 
-@pytest.mark.parametrize("L", L_to_test)
+""" @pytest.mark.parametrize("L", L_to_test)
 @pytest.mark.parametrize("N", N_to_test)
 @pytest.mark.parametrize("J_min", J_min_to_test)
 @pytest.mark.parametrize("lam", lam_to_test)
@@ -16,7 +16,7 @@ def test_reality_synthesis(wavelet_generator, L: int, N: int, J_min: int, lam: i
     f_wav, f_scal = wavelet_generator(L=L, N=N, J_min=J_min, lam=lam)
 
 
-    f = s2let.synthesis_wav2px_real(
+    f = s2let.synthesis_wav2px(
         f_wav.flatten("C"),
         f_scal.flatten("C"),
         lam,
@@ -24,27 +24,27 @@ def test_reality_synthesis(wavelet_generator, L: int, N: int, J_min: int, lam: i
         J_min,
         N,
         0,
-        upsample=True,
+        upsample=True
     )
     f_check = synthesis.synthesis_transform(f_wav, f_scal, L, N, J_min, lam, reality=True)
 
-    np.testing.assert_allclose(f, f_check.flatten("C"), atol=1e-14)
+    np.testing.assert_allclose(f, f_check.flatten("C"), atol=1e-14) """
 
 @pytest.mark.parametrize("L", L_to_test)
 @pytest.mark.parametrize("N", N_to_test)
 @pytest.mark.parametrize("J_min", J_min_to_test)
 @pytest.mark.parametrize("lam", lam_to_test)
 def test_reality_analysis(wavelet_generator, L: int, N: int, J_min: int, lam: int):
-    f = np.random.randn(L, 2 * L - 1) + 1j * np.random.randn(L, 2 * L - 1)
+    f = np.random.randn(L, 2 * L - 1) + 0j * np.random.randn(L, 2 * L - 1)
 
-    f_wav, f_scal = s2let.analysis_px2wav_real(
+    f_wav, f_scal = s2let.analysis_px2wav(
         f.flatten("C"),
         lam,
         L,
         J_min,
         N,
         0,
-        upsample=True,
+        upsample=True
     )
     f_wav_check, f_scal_check = analysis.analysis_transform(f, L, N, J_min, lam, reality=True)
 
