5fb38af1eae06261d8ead815d1935e7f077bf15c
CosmoMatt <mattaprice@hotmail.co.uk>
2023-02-17 14:06:56 +0000

    overhaul setup file to avoid floating numpy import

--- setup.py
+++ setup.py
@@ -1,51 +1,36 @@
-import os
-import shutil
-from setuptools import setup
+from pathlib import Path
+from setuptools import find_packages, setup
 
-import numpy
+this_directory = Path(__file__).parent
 
-# clean previous build
-for root, dirs, files in os.walk("./s2wav/", topdown=False):
-    for name in dirs:
-        if (name == "build"):
-            shutil.rmtree(name)
 
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
+long_description = (this_directory / ".pip_readme.rst").read_text()
+requirements = (
+    (this_directory / "requirements" / "requirements-core.txt").read_text().split("\n")
+)
 
-def read_requirements(file):
-    with open(file) as f:
-        return f.read().splitlines()
-
-def read_file(file):
-   with open(file) as f:
-        return f.read()
-
-long_description = read_file(".pip_readme.rst")
-required = read_requirements("requirements/requirements-core.txt")
-
-include_dirs = [numpy.get_include(),]
-
-extra_link_args=[]
 
 setup(
-    classifiers=['Programming Language :: Python :: 3.6',
-                 'Programming Language :: Python :: 3.7',
-                 'Programming Language :: Python :: 3.8',
-                 'Operating System :: OS Independent',
-                 'Intended Audience :: Developers',
-                 'Intended Audience :: Science/Research'
-                 ],
-    name = "s2wav",
-    version = "0.0.1",
-    prefix='.',
-    url='https://github.com/astro-informatics/s2wav',
-    author='Authors & Contributors',
-    author_email='primary.author.e-mail@ucl.ac.uk',
-    license='GNU General Public License v3 (GPLv3)',
-    install_requires=required,
-    description='A witty project description!',
-    long_description_content_type = "text/x-rst",
-    long_description = long_description,
-    packages=['s2wav']
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+    ],
+    name="s2wav",
+    version="0.0.1",
+    url="https://github.com/astro-informatics/s2wav",
+    author="Authors & Contributors",
+    license="GNU General Public License v3 (GPLv3)",
+    python_requires=">=3.9",
+    install_requires=requirements,
+    description=(
+        "Differentiable and accelerated wavelet transforms with JAX"
+    ),
+    long_description_content_type="text/x-rst",
+    long_description=long_description,
+    packages=find_packages(),
+    include_package_data=True,
+    pacakge_data={"s2wav": ["default-logging-config.yaml"]}
 )
