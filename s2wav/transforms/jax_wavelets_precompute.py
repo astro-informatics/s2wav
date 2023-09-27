487b64fa8c973f954df8d7b38c4e29f80140b456
CosmoMatt <mattaprice@hotmail.co.uk>
2023-09-27 12:11:39 +0100

    add precompute flm_to_analysis

--- s2wav/transforms/jax_wavelets_precompute.py
+++ s2wav/transforms/jax_wavelets_precompute.py
@@ -277,3 +277,103 @@ def analysis(
     else:
         f_scal = spherical.inverse_transform_jax(temp, precomps[1], Ls, sampling, reality, spin, nside)
     return f_wav, f_scal
+
+@partial(jit, static_argnums=(1, 2, 3, 4, 5, 6, 7, 8, 9, 11))
+def flm_to_analysis(
+    flm: jnp.ndarray,
+    L: int,
+    N: int = 1,
+    J_min: int = 0,
+    J_max: int = None,
+    lam: float = 2.0,
+    sampling: str = "mw",
+    nside: int = None,
+    reality: bool = False,
+    multiresolution: bool = False,
+    filters: Tuple[jnp.ndarray] = None,
+    spmd: bool = False,
+    precomps: List[List[jnp.ndarray]] = None,
+) -> Tuple[jnp.ndarray]:
+    r"""Wavelet analysis from pixel space to wavelet space for complex signals.
+
+    Args:
+        f (jnp.ndarray): Signal :math:`f` on the sphere with shape :math:`[n_{\theta}, n_{\phi}]`.
+
+        L (int): Harmonic bandlimit.
+
+        N (int, optional): Upper azimuthal band-limit. Defaults to 1.
+
+        J_min (int, optional): Lowest frequency wavelet scale to be used. Defaults to 0.
+
+        lam (float, optional): Wavelet parameter which determines the scale factor between consecutive wavelet scales.
+            Note that :math:`\lambda = 2` indicates dyadic wavelets. Defaults to 2.
+
+        spin (int, optional): Spin (integer) of input signal. Defaults to 0.
+
+        spin0 (int, optional): Spin (integer) of output signal. Defaults to 0.
+
+        sampling (str, optional): Spherical sampling scheme from {"mw","mwss", "dh", "healpix"}. Defaults to "mw".
+
+        nside (int, optional): HEALPix Nside resolution parameter.  Only required if sampling="healpix".  Defaults
+            to None.
+
+        reality (bool, optional): Whether :math:`f \in \mathbb{R}`, if True exploits
+            conjugate symmetry of harmonic coefficients. Defaults to False.
+
+        multiresolution (bool, optional): Whether to store the scales at :math:`j_{\text{max}}`
+            resolution or its own resolution. Defaults to False.
+
+        filters (Tuple[jnp.ndarray], optional): Precomputed wavelet filters. Defaults to None.
+
+        spmd (bool, optional): Whether to map compute over multiple devices. Currently this
+            only maps over all available devices, and is only valid for JAX implementations.
+            Defaults to False.
+
+        precomps (List[jnp.ndarray]): Precomputed list of recursion coefficients. At most
+            of length :math:`L^2`, which is a minimal memory overhead.
+
+    Returns:
+        f_wav (jnp.ndarray): Array of wavelet pixel-space coefficients
+            with shape :math:`[n_{J}, 2N-1, n_{\theta}, n_{\phi}]`.
+
+        f_scal (jnp.ndarray): Array of scaling pixel-space coefficients
+            with shape :math:`[n_{\theta}, n_{\phi}]`.
+    """
+    if precomps == None:
+        raise ValueError("Must provide precomputed kernels for this transform!")
+
+    J = J_max if J_max is not None else shapes.j_max(L, lam)
+    Ls = shapes.scal_bandlimit(L, J_min, lam, multiresolution)
+
+    f_wav_lmn = shapes.construct_flmn_jax(L, N, J_min, lam, multiresolution)
+    f_wav = shapes.construct_f_jax(
+        L, N, J_min, lam, sampling, nside, multiresolution
+    )
+
+    wav_lm = jnp.einsum(
+        "jln, l->jln",
+        jnp.conj(filters[0]),
+        8 * jnp.pi**2 / (2 * jnp.arange(L) + 1),
+        optimize=True,
+    )
+
+    # Project all wigner coefficients for each lmn onto wavelet coefficients
+    # Note that almost the entire compute is concentrated at the highest J
+    for j in range(J_min, J + 1):
+        Lj, Nj, L0j = shapes.LN_j(L, j, N, lam, multiresolution)
+        f_wav_lmn[j - J_min] = (
+            f_wav_lmn[j - J_min]
+            .at[::2, L0j:]
+            .add(
+                jnp.einsum(
+                    "lm,ln->nlm",
+                    flm[L0j:Lj, L - Lj : L - 1 + Lj],
+                    wav_lm[j, L0j:Lj, L - Nj : L - 1 + Nj : 2],
+                    optimize=True,
+                )
+            )
+        )
+
+        f_wav[j-J_min] = wigner.inverse_transform_jax(f_wav_lmn[j - J_min], precomps[2][j - J_min], Lj, Nj, sampling, reality, nside)
+
+    return f_wav
