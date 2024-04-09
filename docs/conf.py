54e65e029ff560a99db125d8b77a04d07fd45f44
CosmoMatt <mattaprice@hotmail.co.uk>
2024-04-09 15:15:56 +0100

    update top-level aliasing, increment version

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = "2023, Matthew Price, Jason McEwen, Jessica Whitney, Alicja Polanska
 author = "Matthew Price, Jason McEwen, Jessica Whitney, Alicja Polanska"
 
 # The short X.Y version
-version = "0.0.1"
+version = "1.0.1"
 # The full version, including alpha/beta/rc tags
-release = "0.0.1"
+release = "1.0.1"
 
 
 # -- General configuration ---------------------------------------------------
--- s2wav/__init__.py
+++ s2wav/__init__.py
@@ -1,23 +1,24 @@
 # ~~ Core ~~
 from . import filters
 from . import samples
+from . import transforms
 
-# # ~~ Aliases ~~
+# ~~ Aliases ~~
 
-# # JAX recursive transforms
-# from .transforms.wavelet import analysis, synthesis, flm_to_analysis
+# JAX recursive transforms
+from .transforms.wavelet import analysis, synthesis, flm_to_analysis
 
-# # Base transforms
-# from .transforms.base import analysis as analysis_base
-# from .transforms.base import synthesis as synthesis_base
+# Base transforms
+from .transforms.base import analysis as analysis_base
+from .transforms.base import synthesis as synthesis_base
 
-# # JAX precompute transforms
-# from .transforms.wavelet_precompute import analysis as analysis_precomp_jax
-# from .transforms.wavelet_precompute import synthesis as synthesis_precomp_jax
+# JAX precompute transforms
+from .transforms.wavelet_precompute import analysis as analysis_precomp_jax
+from .transforms.wavelet_precompute import synthesis as synthesis_precomp_jax
 
-# # PyTorch precompute transforms
-# from .transforms.wavelet_precompute_torch import analysis as analysis_precomp_torch
-# from .transforms.wavelet_precompute_torch import synthesis as synthesis_precomp_torch
+# PyTorch precompute transforms
+from .transforms.wavelet_precompute_torch import analysis as analysis_precomp_torch
+from .transforms.wavelet_precompute_torch import synthesis as synthesis_precomp_torch
 
-# # Martix precompute functions
-# from .transforms import construct
+# Martix precompute functions
+from .transforms import construct
